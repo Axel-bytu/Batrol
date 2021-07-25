351314b1c143b906b27fb61602f1bf462fd03983
Axel <86326762+Axel-bytu@users.noreply.github.com>
2021-07-25 14:11:25 -0500

    Update toxic.js

--- toxic.js
+++ toxic.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('TOXIC ALIEN\n WHATSAPP BOT\nby toxic Safwan|toxicAlien\nv 2.0.0', {
+CFonts.say('TOXIC BATROL\n WHATSAPP BOT\nby toxic Axel|toxicBatrol\nv 2.0.0', {
   font: 'shade',
   align: 'center',
   gradient: ['red', 'magenta']
