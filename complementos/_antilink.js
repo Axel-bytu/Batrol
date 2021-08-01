4838b5245242339491015467c87d3020def31d66
Axel <86326762+Axel-bytu@users.noreply.github.com>
2021-08-01 13:23:55 -0500

    Update and rename plugins/_antilink.js to complementos/_antilink.js

--- complementos/_antilink.js
+++ complementos/_antilink.js
@@ -7,7 +7,7 @@ handler.before = function (m, { isAdmin, isBotAdmin }) {
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    m.reply('```ᴏᴏᴘs!!, ɴᴏᴛ ᴀʟʟᴏᴡᴅ ʟɪɴᴋ ɢʀᴏᴜᴘ```')
+    m.reply('```Eliminado!!, chao pa seras pateado```')
     if (global.opts['remove']) {
       if (isAdmin || !isBotAdmin) return true
       // this.groupRemove(m.chat, [m.sender])
