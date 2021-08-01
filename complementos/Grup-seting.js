0531ba7be4e6981a26d49061bb464128103f4155
Axel <86326762+Axel-bytu@users.noreply.github.com>
2021-08-01 13:20:46 -0500

    Update and rename plugins/Grup-seting.js to complementos/Grup-seting.js

--- complementos/Grup-seting.js
+++ complementos/Grup-seting.js
@@ -7,7 +7,7 @@ let handler  = async (m, { conn, args, usedPrefix, command }) => {
 	await conn.updatePresence(m.chat, Presence.composing)
 	if (isClose === undefined)
 		throw `
-*ғᴏʀᴍᴀᴛ ᴡʀᴏɴɢ! ᴇxᴀᴍᴘʟᴇ :*
+*¡Listo! Batrol:*
 
   *○ ${usedPrefix + command} close*
   *○ ${usedPrefix + command} open*
