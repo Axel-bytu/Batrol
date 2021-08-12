305681b7620d5cfaa2f9f6af75106d52a293654b
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-12 15:16:06 -0500

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -7,7 +7,7 @@ let handler  = async (m, { conn, args, usedPrefix, command }) => {
 	await conn.updatePresence(m.chat, Presence.composing)
 	if (isClose === undefined)
 		throw `
-*¡Listo! Batrol:*
+*¡Formato erróneo! Ejemplo:*
 
   *○ ${usedPrefix + command} close*
   *○ ${usedPrefix + command} open*
