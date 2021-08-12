bb1d4487c5e5df78bcbd8d2188b689926a2f50ec
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-12 15:10:38 -0500

    Create _autolevelup.js

---
+++ plugins/_autolevelup.js
@@ -0,0 +1,16 @@
+let handler = m => m
+
+let levelling = require('../lib/levelling')
+handler.before = m => {
+	let user = global.DATABASE.data.users[m.sender]
+	if (!user.autolevelup) return !0
+	let before = user.level * 1
+	while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+	if (before !== user.level) m.reply(`
+Felicitaciones, has subido de nivel!
+*${before}* -> *${user.level}*
+Usa *.profile* para verificar
+	`.trim())
+}
+
+module.exports = handler
