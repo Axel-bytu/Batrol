3365b03f1d92487f6cf95e69f430ef74730264a1
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-12 15:39:20 -0500

    Update absen-delete.js

--- plugins/absen-delete.js
+++ plugins/absen-delete.js
@@ -1,7 +1,7 @@
 let handler = async(m, { usedPrefix }) => {
      let id = m.chat
      conn.absent = conn.absent ? conn.absent : {}
-     if (!(id in conn.absen)) throw `_*No absences took place in this group!*_\n\n*${usedPrefix}start absent* - to start absent`
+     if (!(id in conn.absen)) throw `_*No hubo ausencia en este grupo!*_\n\n*${usedPrefix}start absent* - to start absent`
      delete conn.absen[id]
      m.reply(`¡listo!`)
 }
