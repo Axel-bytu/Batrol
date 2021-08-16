b320e840b70e2216fe117387bb5398cc616c08b6
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-15 23:41:56 -0500

    Update addprems.js

--- plugins/addprems.js
+++ plugins/addprems.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (global.prems.includes(who.split`@`[0])) throw 'ᴀʟʀᴇᴅʏ ᴘʀᴇᴍɪᴜᴍ!'
+    if (global.prems.includes(who.split`@`[0])) throw '¡Ya es premium!'
     global.prems.push(`${who.split`@`[0]}`)
     conn.reply(m.chat, `ᴏɪ, @${who.split`@`[0]}. ʏᴏᴜ ᴀʀᴇ ᴀʟʀᴇᴅʏ ᴘʀᴇᴍɪᴜᴍ ᴍᴇᴍʙᴇʀ`, m, {
         contextInfo: {
