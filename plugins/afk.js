695e43223eb34dd95ecae8ccb221b2cd4be8c367
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-15 23:42:48 -0500

    Update afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -3,7 +3,7 @@ let handler = async (m, { text }) => {
   user.afk = + new Date
   user.afkReason = text
   m.reply(`
-${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
+${conn.getName(m.sender)} ahora es AFK${text ? ': ' + text : ''}
 `)
 }
 handler.help = ['afk [reason]']
