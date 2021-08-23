2f74d82c111f8bfba0f4b0714f5f0cf10a3ed980
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-22 22:46:35 -0500

    Delete plugins directory

--- plugins/accbang.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, isAdmin }) => {
-  if (m.fromMe) throw 'nope'
-  if (isAdmin) throw 'Aunque ya soy administrador'
-  await conn.groupMakeAdmin(m.chat, [m.sender])
-}
-handler.command = /^admin.$/i
-handler.rowner = true
-handler.botAdmin = true
-module.exports = handler
