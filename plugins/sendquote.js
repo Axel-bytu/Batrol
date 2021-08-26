9cd168c172e4eb823eb9fb33c3c382e3d3b98b6f
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-25 21:22:20 -0500

    Delete plugins directory

--- plugins/sendquote.js
+++
@@ -1,8 +0,0 @@
-async function handler(m) {
-    if (!m.quoted) throw 'responder mensajes!'
-    let q = this.serializeM(await m.getQuotedObj())
-    if (!q.quoted) throw '¡el mensaje al que está respondiendo no contiene una respuesta!'
-    await q.quoted.copyNForward(m.chat, true)
-}
-handler.command = /^q$/i
-module.exports = handler
