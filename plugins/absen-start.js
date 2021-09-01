c1237cb8a888ddd2851ab07bf6cc01078196cae1
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-31 20:49:13 -0500

    Update absen-start.js

--- plugins/absen-start.js
+++ plugins/absen-start.js
@@ -2,10 +2,10 @@ let handler = async(m, { usedPrefix, text }) => {
      conn.absent = conn.absent ? conn.absent : {}
      let id = m.chat
      if (id in conn.absen) {
-         throw `_*There are still absences in this chat!*_\n\n*${usedPrefix}remove absent* - to delete absences`
+         throw `_*¡Todavía hay ausencias en este chat!*_\n\n*${usedPrefix}remove absent* - para borrar ausencias`
      }
      conn.absent[id] = [
-         m.reply(`Successfully starting timesheet!\n\n*${usedPrefix}absent* - for absent\n*${usedPrefix}checkable* - to check attendance\n*${usedPrefix}remove absent* - to delete attendance data `),
+         m.reply(`¡Inicio exitoso de la hoja de horas!\n\n*${usedPrefix}absent* - por ausente\n*${usedPrefix}checkable* - para comprobar la asistencia\n*${usedPrefix}remove absent* - para borrar los datos de asistencia `),
          [],
          text
      ]
