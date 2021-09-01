387a6dc08a3f3838a72a3965d261f2d03fe722fa
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-31 21:00:00 -0500

    Update addmsg.js

--- plugins/addmsg.js
+++ plugins/addmsg.js
@@ -2,14 +2,14 @@ let { WAMessageProto } = require('@adiwajshing/baileys')
 let handler = async (m, { command, usedPrefix, text }) => {
     let M = WAMessageProto.WebMessageInfo
     let which = command.replace(/add/i, '')
-    if (!m.quoted) throw 'ʀᴇᴘʟʏ ᴍᴇssᴀɢᴇ!'
-    if (!text) throw `ᴜsᴇ *${usedPrefix}list${which}* ᴛᴏ sᴇᴇ ᴛʜᴇ ʟɪsᴛ`
+    if (!m.quoted) throw '¡Responder mensajes!'
+    if (!text) throw `use *${usedPrefix}list${which}* para ver la lista`
     let msgs = global.DATABASE._data.msgs
-    if (text in msgs) throw `'${text}' ʀᴇɢɪsᴛᴇʀᴇᴅ ɪɴ ᴛʜɪs ᴍᴇssᴀɢᴇ ʟɪsᴛ`
+    if (text in msgs) throw `'${text}' registrado en esta lista de mensajes`
     msgs[text] = M.fromObject(await m.getQuotedObj()).toJSON()
-    m.reply(`sᴜᴄᴄᴇsғᴜʟʟʏ ᴀᴅᴅᴇᴅ ᴍᴇssᴀɢᴇ ɪɴ ᴍᴇssᴀɢᴇ ᴀs '${text}'
+    m.reply(`mensaje enviado con éxito en el mensaje como '${text}'
     
-ᴀᴄᴄᴇss ᴡɪᴛʜ ${usedPrefix}get${which} ${text}`)
+acceso con ${usedPrefix}get${which} ${text}`)
 }
 handler.help = ['vn', 'msg', 'video', 'audio', 'img', 'sticker'].map(v => 'add' + v + ' <text>')
 handler.tags = ['database']
