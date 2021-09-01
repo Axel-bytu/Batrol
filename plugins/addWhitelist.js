1d7819a8198cb8218548d5f45518b4bf9233e674
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-08-31 20:53:20 -0500

    Update addWhitelist.js

--- plugins/addWhitelist.js
+++ plugins/addWhitelist.js
@@ -1,7 +1,7 @@
 let handler = async (m, { usedPrefix, command, text, args }) => {
     if (!args || !['add', 'remove'].includes(args[0].toLowerCase())) throw `
-*ᴜsᴀɢᴇ:* ${usedPrefix + command} <add|remove> number,number,...,number
-*ᴇxᴀᴍᴘʟᴇ:*
+*Uso:* ${usedPrefix + command} <add|remove> number,number,...,number
+*Ejemplo:*
 ${usedPrefix + command} add 919567997462
 ${usedPrefix + command} remove 919567997462
 `.trim()
