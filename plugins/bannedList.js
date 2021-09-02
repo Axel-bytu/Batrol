3f78f359eba0a869ce2bc0025deedd1bb2ce3bd9
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-01 21:13:05 -0500

    Update bannedList.js

--- plugins/bannedList.js
+++ plugins/bannedList.js
@@ -3,14 +3,14 @@ let handler = async (m, { conn, usedPrefix }) => {
     let users = Object.entries(global.DATABASE.data.users).filter(user => user[1].banned)
     
     m.reply(`
-┌ *List of Banned Chats*
+┌ *Lista de chats prohibidos*
 │ Total : ${chats.length} Chat${chats ? '\n' + chats.map(([jid], i) => `
 │ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
 │ ${jid}
 `.trim()).join('\n') : ''}
 └────
 
-┌ *List of Banned Users*
+┌ *Lista de usuarios prohibidos*
 │ Total : ${users.length} User${users ? '\n' + users.map(([jid], i) => `
 │ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
 │ ${jid}
