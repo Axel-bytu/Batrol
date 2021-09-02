200c0d83b0ada03ea8c25d932bf9ff4014c50fde
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-01 21:15:52 -0500

    Update banuser.js

--- plugins/banuser.js
+++ plugins/banuser.js
@@ -1,13 +1,13 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text}) => {
-    if (!text) throw 'ᴡʜᴏ ᴡᴀɴᴛs ᴛᴏ ʙᴇ ʙᴀɴɴᴇᴅ?'
+    if (!text) throw 'Quien quiere ser baneado?'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw 'ᴛᴀɢ ᴏɴᴇ'
+    if (!who) throw 'etiqueta uno'
     let users = global.DATABASE._data.users
     users[who].banned = true
-    conn.reply(m.chat, `sᴜᴄᴄᴇsɢᴜʟʟʏ ʙᴀɴɴᴇᴅ`, m)
+    conn.reply(m.chat, `Exitosamente prohibido`, m)
 }
 handler.help = ['ban']
 handler.tags = ['owner']
