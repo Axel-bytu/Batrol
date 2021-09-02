5bc39adcdfa29117b2537260f705fa69aaa0fb5d
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-02 12:13:12 -0500

    Update lockCmdWithMedia.js

--- plugins/lockCmdWithMedia.js
+++ plugins/lockCmdWithMedia.js
@@ -1,9 +1,9 @@
 module.exports = Object.assign(async function handler(m, { command }) {
-    if (!m.quoted) throw 'Reply Message!'
-    if (!m.quoted.fileSha256) throw 'SHA256 Hash Missing'
+    if (!m.quoted) throw '¡Mensaje de respuesta!'
+    if (!m.quoted.fileSha256) throw 'SHA256 Hash Desaparecido'
     let sticker = global.DATABASE._data.sticker
     let hash = m.quoted.fileSha256.toString('hex')
-    if (!(hash in sticker)) throw 'Hash not found in database'
+    if (!(hash in sticker)) throw 'Hash no encontrado en la base de datos'
     sticker[hash].locked = !/^un/i.test(command)
     m.reply('Done!')
 }, {
