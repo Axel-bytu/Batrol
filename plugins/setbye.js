fccfd8ecfbffd3796a16dd06f3b8f787170e4740
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-02 19:43:19 -0500

    Update setbye.js

--- plugins/setbye.js
+++ plugins/setbye.js
@@ -3,8 +3,8 @@ let handler = async(m, { conn, text, isROwner, isOwner }) => {
      if (isROwner) global.conn.bye = text
      else if (isOwner) conn.bye = text
      global.DATABASE._data.chats[m.chat].sBye = text
-     m.reply('Bye set successfully\n@user (Mention)')
-   } else throw 'Wheres the text?'
+     m.reply('Adiós establecido con éxito\n@user (Mention)')
+   } else throw '¿Dónde está el texto?'
 }
 handler.help = ['setbye <text>']
 handler.tags = ['owner', 'group']
