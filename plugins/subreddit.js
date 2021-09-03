7e5fb3f971c78486736536705602dd4ca3e07e86
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-02 20:21:37 -0500

    Update subreddit.js

--- plugins/subreddit.js
+++ plugins/subreddit.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text }) => {
   let res = await fetch(global.API('https://meme-api.herokuapp.com', '/gimme/' + encodeURI(text || defaultSubreddit), {}))
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.url) throw 'Media tidak ditemukan!'
+  if (!json.url) throw '¡Medios no encontrados¡'
   await conn.sendFile(m.chat, json.url, text, json.title, m)
 }
 handler.help = ['subreddit <query>']
