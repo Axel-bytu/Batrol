f535e861ede33f6b98d77e20942b5496ed840326
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-02 19:27:02 -0500

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -3,11 +3,11 @@
 let handler = async(m, { conn, text }) => {
     if (!text) throw 'Please enter a report'
     if (text.length > 300) throw 'text too larg!!!'
-    const laporan = `*「 REPORT 」*\nNomor : wa.me/${m.sender.split`@`[0]}\nPesan : ${text}`
+    const laporan = `*「 REPORTE 」*\nNúmero : wa.me/${m.sender.split`@`[0]}\nMessage : ${text}`
     for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '919567997462@s.whatsapp.net'))
     m.reply(laporan, jid)
     m.reply(laporan, m.sender) // Mwehehehehe
-    m.reply('✔️The problem has been reported to the Owner Bot, fake reports will not be responded to!')
+    m.reply('✔️El problema se ha informado al propietario del bot, ¡no se responderá a los informes falsos!')
 }
 handler.help = ['bug', 'report'].map(v => v + ' <report>')
 handler.tags = ['info']
