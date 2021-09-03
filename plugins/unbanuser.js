484267258384b84044fa0c2066dc16e5c0feda35
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-02 21:15:17 -0500

    Update unbanuser.js

--- plugins/unbanuser.js
+++ plugins/unbanuser.js
@@ -1,13 +1,13 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text}) => {
-    if (!text) throw 'ᴡʜᴏ ᴡᴀɴᴛs ᴛᴏ ʙᴇ ᴜɴʙᴀɴɴᴇᴅ?'
+    if (!text) throw 'Quien quiere ser desbaneado?'
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw 'ᴛᴀɢ ᴏɴᴇ'
+    if (!who) throw 'Etiqueta uno'
     let users = global.DATABASE._data.users
     users[who].banned = false
-    conn.reply(m.chat, `berhasil unbanned`, m)
+    conn.reply(m.chat, `desbanned con éxito`, m)
 }
 handler.help = ['ban']
 handler.tags = ['owner']
