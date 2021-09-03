678d5daa5d5cde6c3ac244f226fc07c3923b3096
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-02 21:01:22 -0500

    Update tomp3.js

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -4,7 +4,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (!/video|audio/.test(mime)) throw `ʀᴇᴘʟʏ ᴛᴏ ᴛʜᴇ ᴠɪᴅᴇᴏ ᴏʀ ᴠᴏɪᴄᴇ ɴᴏᴛᴇ ᴛʜᴀᴛ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴄᴏɴᴠᴇʀᴛ ᴛᴏ ᴍᴘ3 ᴡɪᴛʜ ᴄᴀᴘᴛɪᴏɴ *${usedPrefix + command}*`
+  if (!/video|audio/.test(mime)) throw `responda al video o nota de voz que desea convertir a mp3 con un título *${usedPrefix + command}*`
   let media = await q.download()
   let audio = await toAudio(media, 'mp4')
   conn.sendMessage(m.chat, audio, MessageType.audio, {
