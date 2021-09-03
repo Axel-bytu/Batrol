fcc518eb0873bec3078214b40c993a1d126c48cf
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-02 21:12:43 -0500

    Update unbanchat.js

--- plugins/unbanchat.js
+++ plugins/unbanchat.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   global.DATABASE._data.chats[m.chat].isBanned = false
-  m.reply('ᴜɴʙᴀɴɴᴇᴅ ᴛʜɪs ᴄʜᴀᴛ!')
+  m.reply('Batrol bot activo en chat!')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
