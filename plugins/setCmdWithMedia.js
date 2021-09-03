9fb8d8c7db42bd6fb1318a6080a0cb12535e1961
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-02 19:38:20 -0500

    Update setCmdWithMedia.js

--- plugins/setCmdWithMedia.js
+++ plugins/setCmdWithMedia.js
@@ -1,11 +1,11 @@
 module.exports = Object.assign(async function handler(m, { text }) {
     global.DATABASE._data.sticker = global.DATABASE._data.sticker || {}
-    if (!m.quoted) throw 'Reply Pesan!'
-    if (!m.quoted.fileSha256) throw 'SHA256 Hash Missing'
-    if (!text) throw `Tidak ada teks`
+    if (!m.quoted) throw '¡Mensaje de respuesta!'
+    if (!m.quoted.fileSha256) throw 'SHA256 Hash Desaparecido'
+    if (!text) throw `Sin texto`
     let sticker = global.DATABASE._data.sticker
     let hash = m.quoted.fileSha256.toString('hex')
-    if (sticker[hash] && sticker[hash].locked) throw 'You have no permission to change this sticker command'
+    if (sticker[hash] && sticker[hash].locked) throw 'No tienes permiso para cambiar este comando de etiqueta'
     sticker[hash] = {
         text,
         mentionedJid: m.mentionedJid,
