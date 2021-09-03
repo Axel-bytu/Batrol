868f244bc55e39f036b5741ee41d35a27e273ef5
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-02 21:59:17 -0500

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -2,21 +2,21 @@ let limit = 30
 let fetch = require('node-fetch')
 const { servers, ytv } = require('../lib/y2mate')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'ʜʜʜ ..ᴡʜᴇʀᴇ ɪs ʟɪɴᴋ?'
+  if (!args || !args[0]) throw 'HHH..¿Dónde está el enlace?'
   let chat = global.DATABASE._data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*ᴛɪᴛʟᴇ:* ${title}
-*ғɪʟᴇ sɪᴢᴇ:* ${filesizeF}
+*Título:* ${title}
+*tamaño del archivo:* ${filesizeF}
 `.trim(), m)
   let _thumb = {}
   try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
   catch (e) { }
    conn.sendFile(m.chat, dl_link, title + '.mp4', `
-*ᴛɪᴛʟᴇ:* ${title}
-*ғɪʟᴇ sɪᴢᴇ:* ${filesizeF}
+*Título:* ${title}
+*tamaño del archivo:* ${filesizeF}
 `.trim(), m, false, {
   ..._thumb,
   asDocument: chat.useDocument
