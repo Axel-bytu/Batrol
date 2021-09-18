2389998494b0f7640eac4ae9a0a50e2bc4beb026
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-17 21:06:34 -0500

    Create batrol.js

--- plugins/batrol.js
+++ plugins/batrol.js
@@ -4,16 +4,16 @@ let fetch = require('node-fetch')
 let handler  = async (m, { conn, args, usedPrefix, command }) => {
   if (!args || !args[0]) return conn.sendFile(m.chat, '', '', 'brot', m)
 	let text = args.join` `
-	fetch("https://api.simsimi.net/v1/?text=" + encodeURIComponent(text) + "&lang=es")
+	fetch("https://api.simsimi.net/v2/?text=" + encodeURIComponent(text) + "&lang=es")
   .then(res => res.json())
   .then(batch => {
     conn.updatePresence(m.chat, Presence.composing)
   conn.reply(m.chat, `${batch.success}`, m)
   }) .catch(() => { conn.reply(m.chat, `_¡La función batrol está desabilitada!_`, m) })
 }
-handler.help = ['mami'].map(v => v + ' *texto*')
+handler.help = ['brot'].map(v => v + ' *texto*')
 handler.tags = ['fun']
-handler.command = /^(mami)$/i
+handler.command = /^(brot)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
