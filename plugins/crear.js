96634b691954c775414f5a1353500a284f768fd2
Axel-Broz <86326762+Axel-bytu@users.noreply.github.com>
2021-09-18 17:31:30 -0500

    Create crear.js

---
+++ plugins/crear.js
@@ -0,0 +1,28 @@
+let handler = m => m.reply('*Hola👋*😊, *estos son mis comandos de instalación🙂
+
+$apt update && apt upgrade
+
+$pkg install git -y
+
+$pkg install nodejs -y
+
+$pkg install ffmpeg -y
+
+$pkg install imagemagick -y
+
+$git clone https://github.com/Axel-bytu/Batrol
+
+$cd Batrol
+
+$npm install
+
+$npm start
+Listo luego escaneas el código en whatsapp wed de el número aser bot*')
+
+handler.customPrefix = /install/i
+handler.command = new RegExp
+handler.private = false
+handler.group = true
+handler.owner = true
+
+module.exports = handler
